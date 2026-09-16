6ed9b919a15ca10d38e9239f1241947d1c530770
tvallsales-design <tv.allsales@gmail.com>
2026-09-16 12:35:20 +0200

    Update server.py

--- server.py
+++ server.py
@@ -26,11 +26,11 @@ class Handler(SimpleHTTPRequestHandler):
     def do_GET(self):
         path = urlparse(self.path).path
 
-      
-if path in ("/api/radios", "/api/public/radios"):
-    try:
-        radios = get_radios()
-        data = json.dumps(radios).encode("utf-8")
+        if path in ("/api/radios", "/api/public/radios"):
+            try:
+                radios = get_radios()
+                data = json.dumps(radios).encode("utf-8")
+
                 self.send_response(200)
                 self.send_header("Content-Type", "application/json; charset=utf-8")
                 self.send_header("Access-Control-Allow-Origin", "*")
@@ -44,7 +44,7 @@ if path in ("/api/radios", "/api/public/radios"):
                 self.send_header("Access-Control-Allow-Origin", "*")
                 self.end_headers()
                 self.wfile.write(data)
-            return
+                  return
 
         return super().do_GET()
 
